ed6e79d6ff6b173531b5c781dcf5406235d1f97d
Brian Newsom <brian.newsom@colorado.edu>
2015-10-27 02:09:48 -0600

    Set prior and marginal probabilities calculable w/ Bayes net

---
+++ Assignment6/README.md
@@ -0,0 +1,18 @@
+# Bayes Network
+## Flags
+* -g conditional probability
+* -j joint probability
+* -m marginal probability
+* -p to set a prior for Pollution or Smoking
+
+## Appendix
+
+### Probability 
+
+*Marginal probability*: the probability of an event occurring (p(A)), it may be thought of as an unconditional probability.  It is not conditioned on another event.
+
+*Joint probability*:  p(A and B).  The probability of event A and event B occurring.  It is the probability of the intersection of two or more events.
+
+*Conditional probability*:  p(A|B) is the probability of event A occurring, given that event B occurs. 
+
+- See more at: http://sites.nicholas.duke.edu/statsreview/probability/jmc/#sthash.pMVqlzfo.dpuf
---
+++ Assignment6/main.py
@@ -0,0 +1,96 @@
+import getopt, sys
+
+from bayes import *
+
+def main():
+	net = BayesNet()
+	net.create_network()
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "m:g:j:p:")
+	except getopt.GetoptError as err:
+			# print help information and exit:
+		print str(err) # will print something like "option -a not recognized"
+		sys.exit(2)
+	for o, a in opts:
+		if o in ("-p"):
+			# Split into two values, then set the prior in the bayes net
+			(variable, new_value) = a.split('=')
+			net = set_prior(net, variable, float(new_value))
+		elif o in ("-m"):
+			print calc_marginal(net, a)
+		elif o in ("-g"):
+			print "flag", o
+			print "args", a
+			print type(a)
+			'''you may want to parse a here and pass the left of |
+			and right of | as arguments to calcConditional
+			'''
+			p = a.find("|")
+			print a[:p]
+			print a[p+1:]
+			#calcConditional(a[:p], a[p+1:])
+		elif o in ("-j"):
+			print "flag", o
+			print "args", a
+		else:
+			assert False, "unhandled option"
+		
+    # ...
+
+def set_prior(net, variable, new_value):
+	# Set a marginal probability for smoking or pollution
+	print "Setting prior for variable {0} to {1}".format(variable, new_value)
+	if variable is "P":
+		net.nodes["pollution"].marginal = new_value
+	elif variable is "S":
+		net.nodes["smoker"].marginal = new_value
+
+	# Either way return the new net
+	return net
+
+def calc_marginal(net, arg):
+	# Takes in bayes net and the arg given (which var we want the marginal for)
+	print "Calculating marginal probability for variable {0}".format(arg)
+
+	if arg is "P" or arg is "p":
+		return net.nodes["pollution"]
+
+	elif arg is "S" or arg is "s":
+		return net.nodes["smoker"]
+
+	elif arg is "C" or arg is "c":
+		# We have to actually calculate this time
+		pollution = net.nodes["pollution"]
+		smoker = net.nodes["smoker"]
+
+		cancer = net.nodes["cancer"]
+		# Sum over all possibilities of p and s
+		cancer.marginal = cancer.conditionals["ps"]*pollution.marginal + cancer.conditionals["~ps"]*(1-pollution.marginal)*(smoker.marginal) + cancer.conditionals["p~s"]*pollution.marginal*(1-smoker.marginal) + cancer.conditionals["~p~s"]*(1-pollution.marginal)*(1-smoker.marginal)
+		return cancer
+
+	elif arg is "D" or arg is "d":
+		dyspnoea = net.nodes["dyspnoea"]
+		# Calculate cancers marginal if we don't have it
+		cancer = net.nodes["cancer"]
+		if not cancer.marginal or cancer.marginal is 0:
+			net.nodes["cancer"] = calc_marginal(net, "C")
+			cancer = net.nodes["cancer"]
+		
+		dyspnoea.marginal = dyspnoea.conditionals["c"]*cancer.marginal + dyspnoea.conditionals["~c"]*(1-cancer.marginal)
+		return dyspnoea
+
+	elif arg is "X" or arg is "x":
+		xray = net.nodes["xray"]
+		# Calculate cancers marginal if we don't have it
+		cancer = net.nodes["cancer"]
+		if not cancer.marginal or cancer.marginal is 0:
+			net.nodes["cancer"] = calc_marginal(net, "C")
+			cancer = net.nodes["cancer"]
+		xray.marginal = xray.conditionals["c"]*cancer.marginal + xray.conditionals["~c"]*(1-cancer.marginal)
+
+		return xray
+
+# def calcConditional(var, given)
+
+if __name__ == "__main__":
+    main()
